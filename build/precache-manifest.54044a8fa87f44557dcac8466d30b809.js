75ce2bfd3d7aaae8b0b2ee4143ac9f7d7bd12b25
The Chef <chef@zenprotocol.com>
2019-06-05 04:19:36 +0100

    uploading build

---
+++ build/precache-manifest.54044a8fa87f44557dcac8466d30b809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d1b0d0dd388a467e2a31164e106c53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3103b4a35233629e6f5a",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e887e421d86bc9bf466d",
+    "url": "/static/css/main.6f038941.chunk.css"
+  },
+  {
+    "revision": "3103b4a35233629e6f5a",
+    "url": "/static/js/2.d4822df5.chunk.js"
+  },
+  {
+    "revision": "e887e421d86bc9bf466d",
+    "url": "/static/js/main.75559bb8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
